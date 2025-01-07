b82ddc8309fd0cb9bf937fb64fcadfc2f6d61082
Gowtham <gowthamkomagan0116@gmail.com>
2025-01-07 08:36:47 +0530

    Create TESTBENCH.sv

---
+++ VERIFICATION FOR ADDER IN UVM/TESTBENCH.sv
@@ -0,0 +1,31 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+`include "interface.sv"
+`include "base_test.sv"
+
+module tb_top;
+  bit clk;
+  bit reset;
+  always #2 clk = ~clk;
+  
+  initial begin
+   
+    reset = 1;
+    #5; 
+    reset = 0;
+  end
+  add_if vif(clk, reset);
+  
+  adder DUT(.clk(vif.clk),.reset(vif.reset),.in1(vif.ip1),.in2(vif.ip2),.out(vif.out));
+  
+  initial begin
+    uvm_config_db#(virtual add_if)::set(uvm_root::get(), "*", "vif", vif);
+   
+    $dumpfile("adder.vcd");
+    $dumpvars(0);
+  end
+  initial begin
+    run_test("base_test");
+  end
+endmodule
